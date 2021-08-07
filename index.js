666e0584a8edfca2fc17fb08058352f90f0bf725
nightness <joshguyette@yahoo.com>
2021-08-06 22:57:18 -0500

    drop animation

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import { computerMove, createGrid, dropDisc } from './logic.js'
+import { computerMove, createGrid, dropDisc } from "./logic.js"
 
-const elementGame = document.querySelector('#grid')
-const elementTileTemplate = document.querySelector('#tile-template')
+const elementGame = document.querySelector("#grid")
+const elementTileTemplate = document.querySelector("#tile-template")
 
 const game_config = {
   width: 7,
   height: 6,
   players: 2,
-  min_sequence: 4
+  min_sequence: 4,
 }
 
 let grid = createGrid(game_config)
 loadGrid(grid)
 
 elementGame.onclick = (event) => {
-  if (!event.target.classList.contains('tile')) return
+  if (!event.target.classList.contains("tile")) return
   const discDrop = dropDisc(game_config, grid, event.target.dataset.x)
   if (discDrop) {
     renderDisc(discDrop.location)
-    renderHighlight(discDrop);
-    const computerDrop = computerMove(game_config, grid, discDrop)
-    if (computerDrop) {
-      renderDisc(computerDrop.location)
-      renderHighlight(computerDrop, true);
-    }  
+    renderHighlight(discDrop)
+    setTimeout(() => {
+      const computerDrop = computerMove(game_config, grid, discDrop)
+      if (computerDrop) {
+        renderDisc(computerDrop.location)
+        renderHighlight(computerDrop, true)
+      }
+    }, 1000)
   }
 }
 
@@ -39,15 +41,15 @@ function loadGrid(grid) {
     row.forEach((tile, xIndex) => {
       const elementTile = elementTileTemplate.content
         .cloneNode(true)
-        .querySelector('.tile')
+        .querySelector(".tile")
       elementTile.dataset.x = xIndex
       elementTile.dataset.y = yIndex
       elementTile.dataset.value = tile.value
       elementGame.appendChild(elementTile)
     })
   })
-  elementGame.style.setProperty('--width', game_config.width)
-  elementGame.style.setProperty('--height', game_config.height)
+  elementGame.style.setProperty("--width", game_config.width)
+  elementGame.style.setProperty("--height", game_config.height)
 }
 
 function renderDisc([value, y, x]) {
