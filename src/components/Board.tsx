cc6c1ed5e79900aeed4054a39974547737809132
nightness <joshguyette@yahoo.com>
2021-08-18 14:00:16 -0500

    building out Board and Slot

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,31 @@
 import React, { useContext } from "react"
 import { GridContext } from "."
+import Slot from "./Slot"
+
+interface RowProps {
+  row: number[]
+  x: number
+}
+
+const BoardRow = ({ row, x }: RowProps) => {
+  return (
+    <div>
+      {row.map((value, y) => (
+        <Slot x={x} y={y} value={value} />
+      ))}
+    </div>
+  )
+}
 
 export default function Board() {
-  const { height, width } = useContext(GridContext)
+  const { grid, height, width } = useContext(GridContext)
   return (
-      <div></div>
+    <div>
+      {grid.map((row, x) =>
+        <div>
+          <BoardRow row={row} x={x} />
+        </div>
+      )}
+    </div>
   )
 }
--- src/components/Slot.tsx
+++ src/components/Slot.tsx
@@ -3,14 +3,15 @@ import { GridContext } from './GridContext'
 
 interface Props {
     x: number,
-    y: number
+    y: number,
+    value: number
 }
 
-export default function Slot({ x, y }: Props) {
+export default function Slot({ x, y, value }: Props) {
     const { grid } = useContext(GridContext)
     return (
         <div>
-            
+            {value ? '1' : '0'}
         </div>
     )
 }
