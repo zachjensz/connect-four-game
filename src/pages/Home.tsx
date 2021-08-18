c15ba3b4fa4a098d72004b4c1aca90b1cd9d888d
nightness <joshguyette@yahoo.com>
2021-08-18 15:52:46 -0500

    initial port of titlescreen as Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,15 @@ import { Page } from "../components"
 
 export default function Home() {
   return (
-    <Page>
-      <div>Hello Home Page</div>
-    </Page>
+    <section className="title">
+      <div className="gamemode-select">
+        <button id="dumbot">Dumbot</button>
+        <button id="smartbot">Smartbot</button>
+        <button id="terminator">Terminator</button>
+        <br />
+        <button id="localMultiplayer">Local Multiplayer</button>
+        <button id="onlineMultiplayer">Online Multiplayer</button>
+      </div>
+    </section>
   )
 }
