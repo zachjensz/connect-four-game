132560d77145d8bea2b3b3c10bd361fbbb2821b0
nightness <joshguyette@yahoo.com>
2021-08-19 16:53:12 -0500

    refactoring

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,10 @@ export type Grid = number[][]
 export type Player = 1 | 2
 export type Opponent = "online" | "local" | "ai" | "smart-ai"
 export enum GameStates {
-    PLAYING,
-    WINNER_PLAYER,
-    WINNER_OPPONENT,
-    WINNER_COMPUTER,
-    TIE_GAME
-  }
-  
+  WAITING_FOR_OPPONENT,
+  PLAYING,
+  WINNER_PLAYER,
+  WINNER_OPPONENT,
+  WINNER_COMPUTER,
+  TIE_GAME,
+}
