6246b954885b41238a5c1b3bc4a85f6fcccd17e3
nightness <joshguyette@yahoo.com>
2021-08-19 13:36:05 -0500

    removed webvitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-//@ts-expect-error
-import MetaTags from 'react-meta-tags';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <MetaTags>
-      <title>Hello People</title>
-      <meta name="color-scheme" content="light dark" />
-      <meta name="supported-color-scheme" content="light dark" />
-    </MetaTags> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
@@ -23,8 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
